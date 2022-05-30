94a631785d2fd2bf4d4b76cc145d4385f1f7741f
MiraBellierr <danialzamri0908@gmail.com>
2022-05-30 21:27:27 +0800

    fix show.js

--- server/commands/[✨] utility/show.js
+++ server/commands/[✨] utility/show.js
@@ -1,18 +1,10 @@
-const path = require("path");
+const fs = require("fs");
 
 module.exports = {
 	name: "show",
 	description: "Show the poem",
 	category: "[✨] utility",
 	run: async (client, interaction) => {
-		path.exists(`../../../poems/${interaction.user.id}.php`, (exists) => {
-			if (exists) {
-				return interaction.reply(
-					`https://miraiscute.com/poems/${interaction.user.id}`
-				);
-			}
-
-			return interaction.reply("You haven't create any poem yet");
-		});
+		interaction.reply(`https://miraiscute.com/poems/${interaction.user.id}`);
 	},
 };
