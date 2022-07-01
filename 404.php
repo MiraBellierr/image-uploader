3e4c1868be3ce7cddee820ae5ada606da3d80e5d
MiraBellierr <75848@siswa.unimas.my>
2022-07-01 16:54:50 +0800

    restrict indexes access and add error pages

--- 404.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "not found";
-header("HTTP/1.0 404 Not Found");
-die();
-?>
--- engine.php
+++ engine.php
@@ -2,6 +2,12 @@
 $host=$_SERVER["SERVER_NAME"];
 $query=$_SERVER["QUERY_STRING"];
 $link="https://".$host."/up/".$query; 
+$file_headers = @get_headers($link);
+if(!$file_headers || $file_headers[0] == 'HTTP/1.1 404 Not Found') {
+    header("Location: https://miraiscute.com/404");
+}
+else {
+    return $link;
+}
 
-return $link;
 ?>
