be13ffcd3658a641b1bab742e946eb24d5ec62ed
MiraBellierr <75848@siswa.unimas.my>
2022-07-21 00:25:29 +0800

    update .gitignore

--- poems/548050617889980426.php
+++ poems/548050617889980426.php
@@ -1,5 +1,5 @@
 <?php
-		$file = '548050617889980426.json';
+		$file = '../json/548050617889980426.json';
 		$data = file_get_contents($file);
 		$obj = json_decode($data);
 		$title = $obj->title;
@@ -14,7 +14,7 @@
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<meta name="theme-color" content="#EE82EE" />
 				<meta content="Mira | Poem of the day" property="og:title" />
-				<meta content="Poem of the day by F.s. Yousaf" property="og:description" />
+				<meta content="Poem of the day by mira ミラ ❤✨" property="og:description" />
 				<meta content="miraiscute.com" property="og:site_name" />
 				<meta
 					content="https://cdn.discordapp.com/attachments/873441703330185250/968450734175686666/flower-pot.png"
@@ -24,7 +24,7 @@
 				<meta name="twitter:site" content="miraiscute.com" />
 				<meta name="twitter:creator" content="mira" />
 				<meta name="twitter:title" content="Mira | Poem of the day" />
-				<meta name="twitter:description" content="Poem of the day by F.s. Yousaf" />
+				<meta name="twitter:description" content="Poem of the day by mira ミラ ❤✨" />
 				<meta
 					name="twitter:image"
 					content="https://cdn.discordapp.com/attachments/873441703330185250/968450734175686666/flower-pot.png"
@@ -34,13 +34,13 @@
 					rel="icon"
 					type="image/png"
 					sizes="32x32"
-					href="flower-pot-32x32.png"
+					href="../flower-pot-32x32.png"
 				/>
 				<link
 					rel="icon"
 					type="image/png"
 					sizes="16x16"
-					href="flower-pot-16x16.png"
+					href="../flower-pot-16x16.png"
 				/>
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -49,18 +49,34 @@
 					rel="stylesheet"
 				/>
 				<style>
-					* {
-						font-family: "Alegreya", serif;
-						margin: 0;
-						padding: 0;
-						background-image: url("landscape-1653900045565-3997.jpg");
-					}
+				* {
+					font-family: "Alegreya", serif;
+					margin: 0;
+					padding: 0;
+				}
+	
+				body {
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					height: 100vh;
+					text-align: center;
+					background: url("landscape-1653900045565-3997.jpg") no-repeat center center/ cover;
+	
+				}
+				
+				.inter {
+					padding: 50px;
+					background-color: rgba(0, 0, 0, 0.5);
+					box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+					border-radius: 10px;
+				}
 				</style>
 				<script src="https://cdn.tailwindcss.com"></script>
 			</head>
 			<body>
 				<div
-					class="flex justify-center items-center text-center flex-col"
+					class="flex justify-center items-center text-center min-h-screen flex-col"
 				>
 					<h2 class="font-bold text-3xl text-white"><?php echo $title ?></h2>
 					<br />
